24d38eb3e03856a7c79047c7b3c7521cf18fe0fb
Mattias Lindvall <mattias.lindvall@gmail.com>
2022-02-13 14:47:55 +0100

    add docs

---
+++ docs/conf.py
@@ -0,0 +1,9 @@
+project = "Flask-API-Framework"
+copyright = "2022, Mattias Lindvall"
+author = "Mattias Lindvall"
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+version = "0.0.1"
+release = version
---
+++ docs/index.rst
@@ -0,0 +1,10 @@
+Flask-API-Framework
+===============================================
+
+A framework for building intuitive and semantically correct HTTP API's in Flask.
+
+Install package from PyPI.
+
+.. code-block:: text
+
+    pip install Flask-API-Framework
